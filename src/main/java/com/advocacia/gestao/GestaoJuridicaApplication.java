6cf8b599231e41719d781a931fca84a0d7757f36
AugustodoBRT <augustodobrt@gmail.com>
2026-03-14 15:59:01 -0300

    feat: setup inicial Spring Boot com dependências

---
+++ src/main/java/com/advocacia/gestao/GestaoJuridicaApplication.java
@@ -0,0 +1,13 @@
+package com.advocacia.gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoJuridicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoJuridicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/advocacia/gestao/GestaoJuridicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advocacia.gestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoJuridicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
